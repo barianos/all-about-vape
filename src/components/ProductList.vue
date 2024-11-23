2ed5faa8d76817eb7cba45b090f89aa90a2979d2
Anastasios Barianos <barianos@outlook.com>
2024-11-23 17:36:22 +0200

    limit description to 100 characters

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -19,7 +19,7 @@
 
           <v-card-title>{{ item.producer }}</v-card-title>
           <v-card-subtitle>{{ item.name }}</v-card-subtitle>
-          <v-card-text>{{ item.description }}</v-card-text>
+          <v-card-text>{{ item.description.slice(0, 100) }}...</v-card-text>
         </v-card>
       </v-col>
     </v-row>
