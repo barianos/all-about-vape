3050d9b1a24727c7d328e883516623fbdc1bc836
Anastasios Barianos <barianos@outlook.com>
2024-12-08 13:57:35 +0200

    remove description, add translation, make prettier

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -8,14 +8,14 @@
 
     <v-row v-else-if="products.length > 0">
       <v-col v-for="item in products" :key="item.id" cols="12" sm="12" md="4" lg="3" xl="3">
-        <v-card :to="{ name: 'ProductDetails', params: { productType: item.type, id: item.id } }" class="v-card--link">   
-          <v-img :src="hoveredId === item.id && item.secondary_photo ? item.secondary_photo : item.primary_photo"
+        <v-card :to="{ name: 'ProductDetails', params: { productType: item.type, id: item.id } }" class="v-card--link"> 
+
+          <v-img :lazy-src="item.name" :src="hoveredId === item.id && item.secondary_photo ? item.secondary_photo : item.primary_photo"
             :alt="item.name" @mouseover="handleMouseOver(item.id)" @mouseleave="handleMouseLeave" height="200px"
             class="primary-photo"></v-img>
 
           <v-card-title>{{ item.producer }}</v-card-title>
-          <v-card-subtitle>{{ item.name }}</v-card-subtitle>
-          <v-card-text>{{ item.description.slice(0, 100) }}...</v-card-text>
+          <v-card-text>{{ item.name }}</v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -23,7 +23,7 @@
     <v-row v-else>
       <v-col cols="12">
         <v-alert type="info" border="left" elevation="2" color="secondary">
-          Δεν υπάρχουν διαθέσιμα προϊόντα για αυτήν την κατηγορία.
+          {{ t('noProductsMessage') }}
         </v-alert>
       </v-col>
     </v-row>
@@ -32,8 +32,16 @@
 
 <script>
 import { supabase } from '../supabase';
+import { useI18n } from 'vue-i18n';
 
 export default {
+  setup() {
+    const { t } = useI18n();
+
+    return {
+      t,
+    };
+  },
   props: {
     productType: {
       type: String,
@@ -86,3 +94,28 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.v-card--link:hover {
+  transform: scale(1.03);
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+  transition: transform 0.2s ease, box-shadow 0.2s ease;
+}
+
+
+.v-card {
+  opacity: 0;
+  transform: translateY(20px);
+  animation: fadeInUp 0.3s ease forwards;
+}
+
+@keyframes fadeInUp {
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+
+
+</style>
